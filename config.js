8d54e2f90941f5dbee71788f2b11ab5f87934d10
phatdatle373 <136218473+phatdatle373@users.noreply.github.com>
2024-08-23 13:14:26 +0700

    upload

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //====Nhập Thông Tin Của Bạn Vào Đây==============
 
 //Link Ảnh Background
-ImgLink_Background = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5d2f751b-0791-4cb6-a646-8cfecd441a73/dfyfkj8-98701992-9c3d-484e-8244-e01af07296e5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVkMmY3NTFiLTA3OTEtNGNiNi1hNjQ2LThjZmVjZDQ0MWE3M1wvZGZ5ZmtqOC05ODcwMTk5Mi05YzNkLTQ4NGUtODI0NC1lMDFhZjA3Mjk2ZTUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.8FZvmlLNB6Q2C-ELVExcLxAx8004V27t01M1KO0vP4c"
+ImgLink_Background = "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/mot-em-be-tho-dang-iu-ma-sao-hoi-buon-hiu-do-1-1.jpg"
 
 //Link Ảnh Avatar
-ImgLink_Avatar = "https://i.postimg.cc/76BJFpHV/Ph-t-t-media.jpg"
+ImgLink_Avatar = "https://i.postimg.cc/ZKCcWw9R/S-ng.png"
 
 //Nickname
-Nickname_Profile = "Phát Đạt Media"
+Nickname_Profile = "Đỗ Thị Thảo Sương"
 
 //Tên Thật
-RealName_Profile = "Lê Phát Đạt"
+RealName_Profile = "Khoa Ngoại Ngữ"
 
 //Các Link Của Bạn
 LinkInformation = [
-    Facebook_LinkProfile = "https://www.facebook.com/phatdatle373/",
-    Discord_LinkProfile = "phatdatle373",
-    TikTok_LinkProfile = "https://www.tiktok.com/@le_phat_dat_373",
-    Instagram_LinkProfile = "https://www.instagram.com/phatdatle373//"
+    Facebook_LinkProfile = "https://www.facebook.com/profile.php?id=100030676816283",
+    Discord_LinkProfile = "#",
+    TikTok_LinkProfile = "#",
+    Instagram_LinkProfile = "#"
 ]
